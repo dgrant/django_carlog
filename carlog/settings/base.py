07e61c63ceed045fe697d47d1f5c8644cda785b8
David Grant <davidgrant@gmail.com>
2015-02-06 21:23:03 -0800

    added REST API

--- carlog/settings/base.py
+++ carlog/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'trips',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAdminUser'
+    ],
+    'PAGINATE_BY': 10,
+
+}
--- carlog/urls.py
+++ carlog/urls.py
@@ -1,10 +1,59 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+
+from django.contrib.auth.models import User
+from trips.models import *
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+class CarSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Car
+        fields = ('url', 'name', 'license_plate',)
+
+class TripSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ('url', 'date', 'destination', 'reason', 'distance', 'car',)
+
+class OdometerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Odometer
+        fields = ('url', 'date', 'car', 'km',)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+class OdometerViewSet(viewsets.ModelViewSet):
+    queryset = Odometer.objects.all()
+    serializer_class = OdometerSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'cars', CarViewSet)
+router.register(r'trips', TripViewSet)
+router.register(r'odometers', OdometerViewSet)
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'carlog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'),),
 )
