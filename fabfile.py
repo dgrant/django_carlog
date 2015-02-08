e380d2d7582e80569c55023680dbc7c709b02293
David Grant <davidgrant@gmail.com>
2015-02-07 21:59:05 -0800

    got fabfile working

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,27 @@
 from fabric.api import run, env, cd, shell_env, execute
 
 env.hosts = ['slice:55555']
-ROOT='/home/david/public_html/django/django_recipes/public'
+ROOT='/home/david/public_html/django/django_carlog/public'
 
 def restart():
     with cd(ROOT):
-        run('touch ../../django_recipes.ini')
+        run('touch ../../django_carlog.ini')
 
 def update():
     with cd(ROOT):
         run('git pull --rebase')
 
 def schema():
-    with cd(ROOT), shell_env(DJANGO_SETTINGS_MODULE='django_recipes.settings.production'):
+    with cd(ROOT), shell_env(DJANGO_SETTINGS_MODULE='django_carlog.settings.production'):
         run('env/bin/python ./manage.py migrate')
 
 def backupdb():
     with cd(ROOT):
-        run('./backupLocalDB.sh recipes_django')
+        run('./backupLocalDB.sh carlog_django')
 
 def static():
     with cd(ROOT):
-        run('env/bin/python ./manage.py collectstatic --settings=django_recipes.settings.production --noinput --link --clear')
+        run('env/bin/python ./manage.py collectstatic --settings=django_carlog.settings.production --noinput --link --clear')
 
 def deploy():
     execute(update)
