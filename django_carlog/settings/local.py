8f621647fb411a48e7f937b3eb25f8f77a3e74f8
David Grant <davidgrant@gmail.com>
2017-03-09 22:45:47 -0800

    minor

--- django_carlog/settings/local.py
+++ django_carlog/settings/local.py
@@ -1,7 +1,6 @@
 from .base import *
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
@@ -11,12 +10,8 @@ DATABASES = {
         'PASSWORD': 'carlog_django',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+        'OPTIONS': {
+		'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+	},
     }
 }
-
-#INSTALLED_APPS += ('debug_toolbar',)
-#INTERNAL_IPS = ("127.0.0.1", )
-#DEBUG_TOOLBAR_CONFIG = {'DISABLE_PANELS': ['debug_toolbar.panels.redirects.RedirectsPanel']}
-#MIDDLEWARE_CLASSES += \
-#    ("debug_toolbar.middleware.DebugToolbarMiddleware", )
